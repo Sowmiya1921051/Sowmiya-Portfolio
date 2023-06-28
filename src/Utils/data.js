a19aba8d9c6c4e1ea10e15c85d6b46351866255f
sowmiya <sowmiyanallasivam3434@gmail.com>
2023-06-28 19:24:03 +0530

    first commit

---
+++ src/Utils/data.js
@@ -0,0 +1,181 @@
+export const skills = [
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/174/174854.png",
+        title: "HTML",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/732/732190.png",
+        title: "CSS",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/5968/5968292.png",
+        title: "JavaScript",
+        tag: "frontend"
+    },
+    {
+        icon: "https://cdn-icons-png.flaticon.com/512/760/760457.png",
+        title: "react",
+        tag: "frontend"
+    },
+    {
+        icon: "https://icon2.cleanpng.com/20180517/fxw/kisspng-java-programmer-computer-programming-logo-5afe2f1d9e22d0.3056800815266076456477.jpg",
+        title: "Java",
+        tag: "backend"
+    },
+    {
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM5cTWceW62fObaJjtgG7LTclYmFQncfvNVnewWRCRMw&usqp=CAU&ec=48665698",
+        title: "Python",
+        tag: "backend"
+    },
+    {
+        icon: "https://cdn.iconscout.com/icon/free/png-512/free-django-3628732-3029957.png?f=avif&w=256",
+        title: "Django",
+        tag: "backend"
+    },
+    // {
+    //     icon: "https://cdn-icons-png.flaticon.com/512/5968/5968381.png",
+    //     title: "TypeScript",
+    //     tag: "backend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/color/512/chakra-ui.png",
+    //     title: "Chakra-UI",
+    //     tag: "frontend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/fluency/512/node-js.png",
+    //     title: "Node.js",
+    //     tag: "backend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/color/512/nextjs.png",
+    //     title: "Next.js",
+    //     tag: "frontend"
+    // },
+    // {
+    //     icon: "https://www.svgrepo.com/show/452093/redux.svg",
+    //     title: "redux",
+    //     tag: "backend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/color/512/mongodb.png",
+    //     title: "MongoDB",
+    //     tag: "backend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/?size=512&id=33039&format=png",
+    //     title: "AWS",
+    //     tag: "backend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/color/512/express-js.png",
+    //     title: "Express.js",
+    //     tag: "backend"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/color/512/bootstrap.png",
+    //     title: "Bootstrap",
+    //     tag: "frontend"
+    // },
+    {
+        icon: "https://img.icons8.com/ios-glyphs/512/github.png",
+        title: "Github",
+        tag: "platform"
+    },
+    {
+        icon: "https://i.ibb.co/61nck0S/download-removebg-preview.png",
+        title: "VS Code",
+        tag: "platform"
+    },
+    {
+        icon: "https://img.icons8.com/color/512/git.png",
+        title: "Git",
+        tag: "platform"
+    },
+    // {
+    //     icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/New_Replit_Logo.svg/1200px-New_Replit_Logo.svg.png",
+    //     title: "Replit",
+    //     tag: "platform"
+    // },
+    {
+        icon: "https://img.icons8.com/external-tal-revivo-color-tal-revivo/512/external-codesandbox-an-online-code-editor-and-sharing-web-application-projects-logo-color-tal-revivo.png",
+        title: "Sandbox",
+        tag: "platform"
+    },
+    // {
+    //     icon: "https://img.icons8.com/color/512/firebase.png",
+    //     title: "Firebase",
+    //     tag: "platform"
+    // },
+    // {
+    //     icon: "https://img.icons8.com/ios-filled/512/codepen.png",
+    //     title: "CodePen",
+    //     tag: "platform"
+    // },
+
+]
+
+export const projects = [
+    {
+        id: 0,
+        title: 'ContainerDesign',
+        type: 'Web-Development',
+        image: 'https://github-production-user-asset-6210df.s3.amazonaws.com/93912251/249440800-aec61ba3-11f0-47ba-bd80-540b0cb67c82.png',
+        description: ' I have utilized ReactJS and CSS to create an impressive assortment of container designs. Each design showcases a unique style',
+        techStack: ['Reactjs', 'CSS'],
+        github: 'https://github.com/Sowmiya1921051/Containers-Design',
+        liveDemo: 'https://web-deve-lop-ment.netlify.app/'
+    },
+    {
+        id: 1,
+        title: "PokeMon-API",
+        type: "REST-Api",
+        image: "https://github-production-user-asset-6210df.s3.amazonaws.com/93912251/249443946-feeaac62-7e36-48a2-a682-e2684086d029.png",
+        description: "By leveraging the API's functionalities, I was able to retrieve relevant data for each Pokemon, such as images, abilities, habits, species, and group classifications.",
+        techStack: ["Vite with Reactjs", "CSS"],
+        github: "https://github.com/Sowmiya1921051/PokeMon",
+        liveDemo: "https://po-ke-mon.netlify.app/"
+    },
+    {
+        id: 2,
+        title: 'Login-UX & UI',
+        type: 'Web-Design',
+        image: 'https://github-production-user-asset-6210df.s3.amazonaws.com/93912251/249445612-4206ffbf-a746-448a-bb00-f62cc4eab9ab.png',
+        description: 'Using HTML, CSS, and JavaScript, I created a login page with input fields for username and password, along with a submit button for user authentication.',
+        techStack: ["HTML", "CSS", "JavaScript"],
+        github: 'https://github.com/Sowmiya1921051/LoginPage-Html-CSS',
+        liveDemo: 'https://loginpage-html-css.netlify.app/'
+    },
+    {
+        id: 3,
+        title: "Weather-App",
+        type: "API",
+        image: "https://github-production-user-asset-6210df.s3.amazonaws.com/93912251/249449577-2cda74b3-f217-4f98-87df-a32f96a46ea4.JPG",
+        description: "In API's data, users can easily access essential information like temperature, precipitation, wind, and more, empowering them to make informed decisions and plan accordingly based on the weather forecast.",
+        techStack: ["Reactjs","Tailwind CSS"],
+        github: "https://github.com/Sowmiya1921051/Weather",
+        liveDemo: "https://weather-app-9130.netlify.app/"
+    },
+    {
+        id: 4,
+        title: "Social-media-app",
+        type: "Social-Community",
+        image: "https://github-production-user-asset-6210df.s3.amazonaws.com/93912251/249453655-28ac142f-0a92-40af-b7cc-c4bcf26f37da.png",
+        description: "I have developed a social media app that enables users to create profiles, connect with friends, share posts, photos, and videos, and engage in conversations and interactions with other users, fostering a vibrant and connected online community.",
+        techStack: ["Reactjs", "CSS"],
+        github: "https://github.com/Sowmiya1921051/Social-media-app",
+        liveDemo: "https://social-media-app-deisgn.netlify.app/"
+    },
+    {
+        id: 5,
+        title: "Memes Generator",
+        type: "API",
+        image: "https://github-production-user-asset-6210df.s3.amazonaws.com/93912251/249483631-6020ca8e-534d-4afc-a373-020d9d1039e0.JPG",
+        description: "Memes Generator is the web app that fetch memes from the API using Axios.",
+        techStack: ["Reactjs", "Tailwind CSS"],
+        github: "https://github.com/Sowmiya1921051/Joker",
+        liveDemo: "https://joker-nottu.netlify.app/"
+    },
+]
